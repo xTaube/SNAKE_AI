997e47eaf702eb1f37a4771de8613d560dec91b7
Michal <micmey9@gmail.com>
2022-02-02 15:10:09 +0100

    Implement Agent get_state method

--- game_ai/agent.py
+++ game_ai/agent.py
@@ -18,8 +18,52 @@ class Agent:
         self.gamma = 0  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
 
-    def get_state(self, game):
-        pass
+    @staticmethod
+    def get_state(game: SnakeGameAI) -> np.array:
+        head = game.snake[0]
+        point_l = Point(head.x-20, head.y)
+        point_r = Point(head.x+20, head.y)
+        point_u = Point(head.x, head.y-20)
+        point_d = Point(head.x, head.y+20)
+
+        dir_l = game.direction == DirectionEnum.LEFT
+        dir_r = game.direction == DirectionEnum.RIGHT
+        dir_u = game.direction == DirectionEnum.UP
+        dir_d = game.direction == DirectionEnum.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)),
+
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            # Food location
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y   # food down
+        ]
+
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
         pass
