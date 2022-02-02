85883abd82ae21fedd502e13000d99fee53c895b
Michal <micmey9@gmail.com>
2022-02-02 19:59:40 +0100

    Implement QTrainer

--- game_ai/model.py
+++ game_ai/model.py
@@ -6,7 +6,7 @@ import os
 
 
 class Linear_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
