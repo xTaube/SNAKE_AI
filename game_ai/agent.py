4ae99e6f676a0713e3cbe8136e8fb90bc43bda57
Michal <micmey9@gmail.com>
2022-02-02 15:17:23 +0100

    Implement Agent remenber method

--- game_ai/agent.py
+++ game_ai/agent.py
@@ -66,7 +66,7 @@ class Agent:
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))   # popleft if MAX_MEMORY
 
     def train_long_memory(self):
         pass
